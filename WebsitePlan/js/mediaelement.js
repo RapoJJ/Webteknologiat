e8609af02e88e9f4eff2556894a36a054a4cd6aa
udwee <jonttu.92@hotmail.com>
2019-10-16 13:44:53 +0300

    Interval works, no stop functions implemented

--- WebsitePlan/js/mediaelement.js
+++ WebsitePlan/js/mediaelement.js
@@ -4,26 +4,37 @@ var dogImg_array = JSON.parse(dogs_json);
 
 
 window.onload = function() {
+    console.log(this.dogImg_array);
     document.getElementById('currentImg').src = this.dogImg_array[0].src;
+    document.getElementById('imgtitle').innerHTML = this.dogImg_array[0].name;
 }
 
-function nextImage(element)
-{
-    var img = document.getElementById(element);
-    console.log(img);
-    for(var i = 0; i < dogImg_array.length;i++)
-    {
-        console.log(dogImg_array[i].src);
-        console.log(img.src);
-        
-        if(i === dogImg_array.length - 1){
-            document.getElementById(element).src = dogImg_array[0].src;
-            break;
-            }
-        else {
-            document.getElementById(element).src = dogImg_array[i+1].src;
-            break;
-            }
+var imageIndex = 0;
+var imgAmount = dogImg_array.length;
+var img = document.getElementById('currentImg');
+var title = document.getElementById('imgtitle');
+
+function nxtImg() { 
+    imageIndex++;
+    if (imageIndex == imgAmount) {
+        imageIndex = 0;
     }
+    img.src = dogImg_array[imageIndex].src;
+    title.innerHTML = dogImg_array[imageIndex].name;
+    console.log(imageIndex)
     
+}
+
+function bckImg() {
+    imageIndex--;
+    if (imageIndex < 0) {
+        imageIndex = imgAmount - 1;
+    }
+    img.src= dogImg_array[imageIndex].src;
+    title.innerHTML = dogImg_array[imageIndex].name;
+    console.log(imageIndex);
+}
+
+function autoplay() {
+    var intervalId = setInterval(nxtImg, 2000);
 }
