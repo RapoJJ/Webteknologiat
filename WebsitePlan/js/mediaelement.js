8aa3a4ce85b8cf963a139f8b196ce0effa681677
udwee <jonttu.92@hotmail.com>
2019-10-18 13:48:36 +0300

    carousel works

--- WebsitePlan/js/mediaelement.js
+++ WebsitePlan/js/mediaelement.js
@@ -1,20 +1,21 @@
-
 var dogs_json = '[{"name":"Head tilt", "src":"image/headtilt.jpg"}, {"name":"Nala sits", "src":"image/sit.png"}, {"name":"Nala Stares", "src":"image/stare.jpg"}, {"name":"Nala stares up", "src":"image/mighty.jpg"}]';
 var dogImg_array = JSON.parse(dogs_json);
+var localStorageKey = "index";
 
-
-window.onload = function() {
-    console.log(this.dogImg_array);
-    document.getElementById('currentImg').src = this.dogImg_array[0].src;
-    document.getElementById('imgtitle').innerHTML = this.dogImg_array[0].name;
-}
-
-var imageIndex = 0;
+var imageIndex;
 var imgAmount = dogImg_array.length;
 var img = document.getElementById('currentImg');
-var title = document.getElementById('imgtitle');
+var title = document.getElementById('imgtext');
 
-function nxtImg() { 
+
+window.onload = function () {
+    this.imageIndex = 0;
+    console.log(this.dogImg_array);
+    document.getElementById('currentImg').src = this.dogImg_array[0].src;
+    document.getElementById('imgtext').innerHTML = this.dogImg_array[0].name;
+}
+
+function nxtImg() {
     imageIndex++;
     if (imageIndex == imgAmount) {
         imageIndex = 0;
@@ -22,7 +23,6 @@ function nxtImg() {
     img.src = dogImg_array[imageIndex].src;
     title.innerHTML = dogImg_array[imageIndex].name;
     console.log(imageIndex)
-    
 }
 
 function bckImg() {
@@ -30,11 +30,26 @@ function bckImg() {
     if (imageIndex < 0) {
         imageIndex = imgAmount - 1;
     }
-    img.src= dogImg_array[imageIndex].src;
+    img.src = dogImg_array[imageIndex].src;
     title.innerHTML = dogImg_array[imageIndex].name;
     console.log(imageIndex);
 }
 
+var intervalId;
+
 function autoplay() {
-    var intervalId = setInterval(nxtImg, 2000);
+    if (intervalId != null) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+    else {
+        intervalId = setInterval(nxtImg, 2000);
+    }
 }
+
+document.getElementById("play").addEventListener("click", function (e) {
+    var target = e.target;
+
+    target.classList.toggle("fa-pause");
+    target.classList.toggle("fa-play");
+}, false);
